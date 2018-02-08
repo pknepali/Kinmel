7873a608d7dcc8d9877b6f93aef1f43a5a015b21
Prakash Bastola <prakash_bastola@intuit.com>
2018-02-08 15:06:33 -0800

    updated package.json to build and run server with same command

--- src/test.js
+++ src/test.js
@@ -1,1 +1,1 @@
-module.exports = "Peace and Propsperity everywhere and for everyone and all the time";
+module.exports = "Peace and Propsperity everywhere and for everyone and all the time.\n\n Heaven on Earth.\n\n Lets make it happen.\n\nGod Bless";
